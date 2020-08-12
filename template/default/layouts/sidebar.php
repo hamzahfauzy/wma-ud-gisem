65add8c05ac6b26435ba4b3dba5c85de896c6fc4
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-08-12 16:36:51 +0700

    Fix logo error

--- template/default/layouts/sidebar.php
+++ template/default/layouts/sidebar.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-light-success elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="<?=base_url()?>" class="brand-link">
       <img src="<?=asset('img/AdminLTELogo.png')?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light"><?= app('application_name')?></span>
